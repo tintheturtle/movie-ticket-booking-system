55f46f261c6d761ed0852927c87acc63baaf1563
Matt Firmin <77645801+firminm@users.noreply.github.com>
2021-12-03 17:38:40 -0500

    Added multiple constructors

--- src/com/system/ShowTime.java
+++ src/com/system/ShowTime.java
@@ -1,4 +1,20 @@
+import java.util.*;
+
 public class ShowTime{
 
-    public ShowTime(){}
+    String day;
+    ArrayList<double> times;
+
+    public ShowTime(String day, double time){
+        this.day = day;
+        this.times = new ArrayList<double>(time);
+    }
+
+    @override
+    public ShowTime(String day, ArrayList<double> times){
+        this.day = day;
+        this.time = times
+    }
+
+    
 }
