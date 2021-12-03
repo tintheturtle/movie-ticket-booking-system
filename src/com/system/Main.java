84ff0b84ff1df119c73955fb0e9bb9e6508b3d2a
Matt Firmin <77645801+firminm@users.noreply.github.com>
2021-12-03 18:29:26 -0500

    Added main page and login page skeleton (non-functional)

--- src/com/system/Main.java
+++ src/com/system/Main.java
@@ -2,7 +2,38 @@ package com.system;
 
 public class Main {
 
+    static Scanner kb = new Scanner(System.in);
+    static String PASSWORD = "password";
+
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    public int mainPage(){
+        System.out.println("\n\n\n-------------------------");
+        System.out.println("-  MAIN PAGE - TYPE CHOICE TO ADVANCE");
+        System.out.println("-  1. VIEW SHOWINGS/PURCHASE TICKET");
+        System.out.println("-  2. EMPLOYEE LOGIN\n");
+        int choice = kb.nextInt();
+        if (choice != 1 or choice != 2)
+            return -1;
+        return choice;
+    }
+
+
+    public boolean login(){
+        System.out.println("\n\n\n-------------------------"); // 25 -
+        System.out.println("-  EMPLOYEE LOGIN");
+        System.out.print("\n-  PASSWORD: ");
+        String pass = kb.nextLine();
+        if (pass.equals(PASSWORD))
+            return true;
+        else{
+            System.out.println("INCORRECT LOGIN. RETRY? (Y/N)");
+            if (kb.next().equals('Y'))
+                return login();
+            else
+                return false;
+        }
     }
 }
