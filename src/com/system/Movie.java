b405b8608c9244cb0d032c005c5b50d7c05a5fb1
Matt Firmin <77645801+firminm@users.noreply.github.com>
2021-12-03 17:31:50 -0500

    Created Movie.java class

---
+++ src/com/system/Movie.java
@@ -0,0 +1,26 @@
+import ShowTime;
+
+public class Movie{
+
+    public static ArrayList<Movie> movieList = new ArrayList<Movie>();
+
+    String title;
+    double price;
+    Arraylist<ShowTimes> showtimes;
+
+
+    public Movie(String title, double price, ArrayList<ShowTime> showtimes){
+        this.title = title;
+        this.price  = price;
+        this.showtimes = showtimes;
+
+        this.movieList.add(this.Movie)
+    }
+
+    public String getTitle(){ return this.title; }
+    public double getPrice(){ return this.price; }
+    public ArrayList<ShowTime> getShowTimes(){ return this.showtimes; }
+    
+    public void setPrice(double price){ this.price = price; }
+    public void addShowtime(ShowTime newTime){ this.showtimes.add(newTime); }
+}
