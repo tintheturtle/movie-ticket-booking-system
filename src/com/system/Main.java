fa411a94e6f4b77c5b34791b44473476f20c3a73
Matt Firmin <77645801+firminm@users.noreply.github.com>
2021-12-03 22:24:50 -0500

    Revamped menu system using try/catch. Still needs command implementation

--- src/com/system/Main.java
+++ src/com/system/Main.java
@@ -6,22 +6,105 @@ public class Main {
     static String PASSWORD = "password";
 
     public static void main(String[] args) {
-	// write your code here
-    }
+        while (true) // Encloses entire function
+        {
+            System.out.println("\n\n\n------------------------------------------------"); 
+            System.out.println("--  WELCOME TO OUR MOVIE TICKET BOOKING SYSTEM  --");
+            System.out.println("--------------------------------------------------");
+            System.out.println("--  1. VIEW SHOWINGS/PURCHASE TICKET            --");
+            System.out.println("--  2. EMPLOYEE LOGIN                           --");
+            System.out.println("--------------------------------------------------");
 
-    public int mainPage(){
-        System.out.println("\n\n\n-------------------------");
-        System.out.println("-  MAIN PAGE - TYPE CHOICE TO ADVANCE");
-        System.out.println("-  1. VIEW SHOWINGS/PURCHASE TICKET");
-        System.out.println("-  2. EMPLOYEE LOGIN\n");
-        int choice = kb.nextInt();
-        if (choice != 1 or choice != 2)
-            return -1;
-        return choice;
-    }
+            int menuChoice = kb.nextInt();
+            switch (menuChoice)
+            {
+                case 1: 
+                    clearscreen();
+                    int customerChoice;
+                    do {    // encloses full case 1
+                        System.out.println("\n\n\n------------------------------------------------"); 
+                        System.out.println("--  WELCOME TO OUR MOVIE TICKET BOOKING SYSTEM  --");
+                        System.out.println("--------------------------------------------------");
+                        System.out.println("--  1. See Movie Showtimes                      --");
+                        System.out.println("--  2. Book Ticket                              --");
+                        System.out.println("--  3. View your Ticket(s)                      --");
+                        System.out.println("--  4. Cancel Ticket                            --");
+                        System.out.println("--  5. Back                                     --");
+                        System.out.println("--------------------------------------------------");
+                        
+                        customerChoice = kb.nextInt();
+                        switch (customerChoice)
+                        {
+                            case 1:
+                                ;
 
+                            case 2:
+                                ;
+
+                            case 3:
+                                ;
+                            case 4:
+                                ;
+                            case 5:
+                                break;  // actual implementation
+                            default:
+                                System.out.println("\n\nPLEASE ENTER A VALID CHOICE");
+                                customerChoice = 0;
+                        }
+                    } while (customerChoice == 0); // Set in error case
+                    break;
+
+                
+                case 2:
+                    if (login()){
+                        int employeeChoice;
+                        do{
+                            clearscreen();
+                            System.out.println("\n\n\n------------------------------------------------"); 
+                            System.out.println("----------------  Employee Panel   ---------------");
+                            System.out.println("--------------------------------------------------");
+                            System.out.println("--  1. See Movie Showtimes                      --");
+                            System.out.println("--  2. Add Movie                                --");
+                            System.out.println("--  3. Add New Showtime                         --");
+                            System.out.println("--  4. See Issued Tickets                       --");
+                            System.out.println("--  5. Back                                     --");
+                            System.out.println("--------------------------------------------------");
+
+                            employeeChoice = kb.nextInt();
+                            switch (employeeChoice)
+                            {
+                                case 1:
+                                    ;
+                                case 2:
+                                    ;
+                                case 3:
+                                    ;
+                                case 4:
+                                    ;
+                                case 5:
+                                    break; // actual implementation
+                                default:
+                                    System.out.println("\n\nPLEASE ENTER A VALID CHOICE");
+                                    employeeChoice = 0;
+                            }
+                        } while (employeeChoice == 0);
+                    }   // if login()
+                    else{
+                        clearscreen();
+                        System.out.println("\n\n\n------------------------------------------------"); 
+                        System.out.println("----------------  INVALID ACCESS   ---------------");
+                        System.out.println("--------------------------------------------------");    
+                    }
+                    break;  // Needs to be double-checked... might need to be after the most recent do/while loop instead
+                
+                default:
+                    System.out.println("\n\nPLEASE ENTER A VALID CHOICE");
+            }
+        }
+    }
 
     public boolean login(){
+        clearscreen();
         System.out.println("\n\n\n-------------------------"); // 25 -
         System.out.println("-  EMPLOYEE LOGIN");
         System.out.print("\n-  PASSWORD: ");
@@ -29,7 +112,7 @@ public class Main {
         if (pass.equals(PASSWORD))
             return true;
         else{
-            System.out.println("INCORRECT LOGIN. RETRY? (Y/N)");
+            System.out.print("\n\nINCORRECT LOGIN. RETRY? (Y/N): ");
             if (kb.next().equals('Y'))
                 return login();
             else
