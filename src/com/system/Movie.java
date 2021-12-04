768f480d3727d89ee39cfbc29a12a07363061422
Matt Firmin <77645801+firminm@users.noreply.github.com>
2021-12-03 22:46:33 -0500

    Changed MovieList to HashMap, added static methods

--- src/com/system/Movie.java
+++ src/com/system/Movie.java
@@ -3,19 +3,27 @@ import ShowTime;
 
 public class Movie{
 
-    public static ArrayList<Movie> movieList = new ArrayList<Movie>();
+    // Associates Movie title to Movie obj
+    public static HashMap<String, Movie> movieMap = new HashMap<String, Movie>();
+    // public static ArrayList<Movie> movieList = new ArrayList<Movie>();
 
     String title;
     double price;
     Arraylist<ShowTimes> showtimes;
-
 
     public Movie(String title, double price, ArrayList<ShowTime> showtimes){
         this.title = title;
         this.price  = price;
         this.showtimes = showtimes;
 
-        this.movieList.add(this.Movie)
+        this.movieMap.put(title, this.Movie)
+    }
+
+    public static Movie getMovie(String title){
+        return movieMap.get(title);
+    }
+    public static ArrayList<Movie> getMovieList(){ 
+        return (new ArrayList<Movie>(movieMap.values()));
     }
 
     public String getTitle(){ return this.title; }
@@ -24,4 +32,5 @@ public class Movie{
     
     public void setPrice(double price){ this.price = price; }
     public void addShowtime(ShowTime newTime){ this.showtimes.add(newTime); }
+
 }
