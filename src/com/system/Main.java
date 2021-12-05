e639dee753aad94dd174a98714c03c90755d7f4f
tintheturtle <tinhoang@bu.edu>
2021-12-05 09:40:21 -0500

    attempting to add database driver

--- src/com/system/Main.java
+++ src/com/system/Main.java
@@ -1,8 +1,28 @@
 package com.system;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Connection conn = null;
+
+        try {
+            conn =
+                    DriverManager.getConnection("jdbc:mysql://localhost/testdb?" +
+                            "user=root&password=Sud0Whodo!3");
+
+            // Do something with the Connection
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
 	// write your code here
     }
 }
