a7bb01d7f0a3f81cf3915dd4ade3e94266f3e2e9
tintheturtle <tinhoang@bu.edu>
2021-12-06 12:23:25 -0500

    added validator class

--- src/com/system/Validator.java
+++ src/com/system/Validator.java
@@ -1,4 +1,25 @@
 package com.system;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Validator {
+
+    public boolean validDate(String date) {
+        try {
+            new SimpleDateFormat("DD/mm/yyyy").parse(date);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+
+    public boolean validTime(String time) {
+        try {
+            new SimpleDateFormat("HH:mm").parse(time);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
 }
