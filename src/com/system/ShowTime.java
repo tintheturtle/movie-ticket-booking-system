cf8950bac5263ac2adc3000a32614f448fc3fba3
Matt Firmin <77645801+firminm@users.noreply.github.com>
2021-12-05 19:20:56 -0500

    added method that displays showtimes for a specific movie

--- src/com/system/ShowTime.java
+++ src/com/system/ShowTime.java
@@ -18,6 +18,7 @@ public class ShowTime{
         showList.add(addedMovie);
 
         // If movie is already in map then add movie to the list of times, otherwise just create a new list
+        // This doesn't appear to do anything... delete? -- matt
         if (movieMap.containsKey(title)) {
             ArrayList<Movie> movieTimes = movieMap.get(title);
             movieTimes.add(addedMovie);
@@ -38,12 +39,17 @@ public class ShowTime{
         }
     }
 
-    public void displayMovieShowtimes(String title)
-    {
 
+    public boolean displayShowtimeForMovie(String title)
+    {
         for (Movie show : showList) {
-            System.out.printf("%-20s %-20s\n", show.title, show.time);
+            if (show.title.equals(title)){
+                System.out.printf("%-20s %-20s\n", show.title, show.time);
+                return true;
+            }
         }
+        System.out.println("Movie could not be found, press any key to return to main menu");
+        return false;
     }
 
     
