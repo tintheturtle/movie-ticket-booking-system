043cc3d857274e0c6db9edd4c6b6c26b2d3a1aa3
Matt Firmin <77645801+firminm@users.noreply.github.com>
2021-12-07 14:40:55 -0500

    added methods addMovie(), removeMovie()

--- src/com/system/Employee.java
+++ src/com/system/Employee.java
@@ -2,11 +2,19 @@ package com.system;
 
 import java.io.IOException;
 
-public class Employee {
+public class Employee extends Person{
 
-
-    public static void main(String[] args) throws InterruptedException, IOException {
-
-
+    public Employee(String employeeID){
+        super(employeeID);
     }
+
+    public void addMovie(ShowTime showtimes, String title, String day, String time){
+        showtimes.addMovie(title, day, time);   // Potentially have a Movie.addedBy(employeeID) attrib  ?
+    }
+
+    public void removeMovie(ShowTime showtimes, String title){
+        showtimes.removeMovie(title);
+    }
+
+
 }
