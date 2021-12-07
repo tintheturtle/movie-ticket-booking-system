f7a42ddf876b91ab4a7a9070ccc1278049ab3edd
Matt Firmin <77645801+firminm@users.noreply.github.com>
2021-12-07 14:38:57 -0500

    Created Person superclass

---
+++ src/com/system/Person.java
@@ -0,0 +1,18 @@
+package com.system;
+
+import java.io.IOException;
+
+public class Person {
+    String userID;
+
+    public Person(String userID){
+        this.userID = userID;
+    }
+
+    
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+
+
+    }
+}
